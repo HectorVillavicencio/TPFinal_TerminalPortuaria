0161461ec8e81f6db58fcef1585cd0250a4af977
vgianfranco-git <gianfranco.ventola@gmail.com>
2023-12-07 17:21:07 -0300

    test de servicios

--- test/ar/edu/unq/po2/tpFinal/Servicio/ServicioTest.java
+++ test/ar/edu/unq/po2/tpFinal/Servicio/ServicioTest.java
@@ -9,8 +9,12 @@ import org.junit.jupiter.api.Test;
 
 import ar.edu.unq.po2.TPFinal_TerminalPortuaria.container.Container;
 import ar.edu.unq.po2.TPFinal_TerminalPortuaria.container.TipoContainer;
-
-
+import ar.edu.unq.po2.TPFinal_TerminalPortuaria.transporteTerrestre.Camion;
+import ar.edu.unq.po2.TPFinal_TerminalPortuaria.transporteTerrestre.Chofer;
+import ar.edu.unq.po2.tpFinal.orden.*;
+import ar.edu.unq.po2.tpFinal.NavieraYCircuito.Viaje;
+import ar.edu.unq.po2.tpFinal.ShipperYConsignee.Shipper;
+import ar.edu.unq.po2.tpFinal.TerminalPortuaria.*;
 
 
 class ServicioTest {
@@ -19,34 +23,54 @@ class ServicioTest {
 	private Lavado servicioLavado;
 	private Servicio servicioPesado;
 	private Container container1;
+	private OrdenDeExportacion ordenDeExportacion;
+	private Shipper shipper;
+	private Camion camion;
+	private Camion camion2;
+	private Chofer chofer;
+	private Chofer chofer2;
+	private Viaje viaje;
+	private int hsAntesDeTurno;
+	private TerminalPortuaria terminal;
+	private TerminalPortuaria terminal2;
 	
 	
 	@BeforeEach
 	void SetUp() throws Exception{
-		servicioAlmacenamiento= new AlmacenamientoExcedente(2,100);
-		servicioElectricidad=new Electricidad(30);
-		servicioLavado= new Lavado(100,  50, 70);
-		servicioPesado=new Pesado(10);
 		container1 = mock(Container.class);
+		terminal = mock(TerminalPortuaria.class);
+		terminal2 = mock(TerminalPortuaria.class);
+		shipper = mock(Shipper.class);
+		camion = mock(Camion.class);
+		camion2 = mock(Camion.class);
+		chofer = mock(Chofer.class);
+		chofer2 = mock(Chofer.class);
+		viaje = mock(Viaje.class);
+		hsAntesDeTurno = 2;
+		ordenDeExportacion = new OrdenDeExportacion(shipper,container1,camion,chofer,viaje, hsAntesDeTurno,terminal,terminal2);
+		servicioAlmacenamiento= new AlmacenamientoExcedente(2,100);
+		servicioElectricidad=new Electricidad(30,ordenDeExportacion);
+		servicioLavado= new Lavado(100,  50, ordenDeExportacion);
+		servicioPesado=new Pesado(10);
 	}
 
 	@Test
 	void testServicioAlmacenamientoConDiasExcedentes() {
-		assertEquals(servicioAlmacenamiento.costo(container1),200);
+		assertEquals(servicioAlmacenamiento.costo(10),200);
 	}
 	
 	@Test
 	void testServicioElectricidadConConsumo() {
-		when(container1.getTipoContainer()).thenReturn(TipoContainer.Reefer);
-		when(container1.getConsumo()).thenReturn(10.0);
-		assertEquals(servicioElectricidad.costo(container1),300);
+		when(ordenDeExportacion.getContainer().getTipoContainer()).thenReturn(TipoContainer.Reefer);
+		when(ordenDeExportacion.getContainer().getConsumo()).thenReturn(10.0);
+		assertEquals(servicioElectricidad.costo(10),300);
 	}
 	
 	@Test
 	void testServicioElectricidadContainerNoReefer() {
 		when(container1.getTipoContainer()).thenReturn(TipoContainer.Dry);
 		when(container1.getConsumo()).thenReturn(0.0);
-		assertEquals(servicioElectricidad.costo(container1),0);
+		assertEquals(servicioElectricidad.costo(10),0);
 	}
 	
 	@Test
@@ -54,8 +78,8 @@ class ServicioTest {
 		when(container1.getAltura()).thenReturn(10.0);
 		when(container1.getAncho()).thenReturn(10.0);
 		when(container1.getLargo()).thenReturn(10.0);
-		assertEquals(servicioLavado.totalMetroCubico(container1),1000);
-		assertEquals(servicioLavado.costo(container1),100);
+		when(ordenDeExportacion.getContainer().getMetrosCubicos()).thenReturn(30.0);
+		assertEquals(servicioLavado.costo(10),50);
 	}
 	
 	@Test
@@ -63,12 +87,12 @@ class ServicioTest {
 		when(container1.getAltura()).thenReturn(5.0);
 		when(container1.getAncho()).thenReturn(5.0);
 		when(container1.getLargo()).thenReturn(2.0);
-		assertEquals(servicioLavado.costo(container1),50);
+		assertEquals(servicioLavado.costo(10),50);
 	}
 	
 	@Test
 	void testServicioPesado() {
-		assertEquals(servicioPesado.costo(container1),10);
+		assertEquals(servicioPesado.costo(10),10);
 	}
 
 }
